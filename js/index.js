20d964e13c9e2a5e1c938c5f3034a5bfbd9f4095
sjpet24 <u1348136@umail.utah.edu>
2024-10-28 16:35:55 -0600

    second commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+$("#bookcases").on("click", function(){
+    $(".hideMe1").slideToggle();
+});
+$("#bedroom").on("click", function(){
+    $(".hideMe2").slideToggle();
+});
+$("#couches").on("click", function(){
+    $(".hideMe3").slideToggle();
+});
+$("#patio").on("click", function(){
+    $(".hideMe4").slideToggle();
+});
+$("#desks").on("click", function(){
+    $(".hideMe5").slideToggle();
+});
